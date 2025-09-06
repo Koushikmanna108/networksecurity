06e1742c5a073ea4dca1f9f6e4d327b2074fc9ea
Koushik Manna <kmanna713@gmail.com>
2025-09-06 21:24:23 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import sys
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.entity.config_entity import(
+     DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+)
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 if __name__ == "__main__":
     try:
-        trainingpipelineconfig = TrainingPipelineConfig()
-        dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
-        data_ingestion = DataIngestion(dataingestionconfig)
-        logging.info("Initiate data ingestion ")
+        training_pipeline_config = TrainingPipelineConfig()
+        data_ingestion_config = DataIngestionConfig(training_pipeline_config)
+        data_ingestion = DataIngestion(data_ingestion_config)
+        logging.info("Initiate Data Ingestion ")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
-        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        print(dataingestionartifact)
+        data_validation_config = DataValidationConfig(training_pipeline_config)
         data_validation = DataValidation(dataingestionartifact,data_validation_config)
         logging.info("Initiate Data Validation")
-        datavalidationartifact = data_validation.initiate_data_validation()
+        data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
-        print(datavalidationartifact)
+        print(data_validation_artifact)
+        data_transformation_config = DataTransformationConfig(training_pipeline_config)
+        data_transformation = DataTransformation(data_validation_artifact,data_transformation_config)
+        logging.info("Initiate Data Transformation")
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("Data Transformation Completed")
+        print(data_transformation_artifact)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -6,7 +6,7 @@ import pandas as pd
 '''
 Defining common constant variable for training pipeline
 '''
-TARGET_COLUMNS = "Result"
+TARGET_COLUMN = "Result"
 PIPELINE_NAME : str = "NetworkSecurity"
 ARTIFACT_DIR : str = "Artifacts"
 FILE_NAME : str = "phisingData.csv"
@@ -34,3 +34,18 @@ DATA_VALIDATION_VALID_DIR : str = "validated"
 DATA_VALIDATION_INVALID_DIR : str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR : str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME : str = "report.yaml"
+
+
+'''
+Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
+'''
+DATA_TRANSFORMATION_DIR_NAME : str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR : str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR : str = "transformed_object"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+## KNN imputer to replace nan values
+DATA_TRANSFORMATION_IMPUTER_PARAMS : dict = {
+    "missing_values" : np.nan,
+    "n_neighbors" : 3,
+    "weights" : "uniform"
+}
